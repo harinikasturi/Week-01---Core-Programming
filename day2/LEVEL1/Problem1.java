bab622fa7c3cae6b0872c5e7cab5eb8c4c639af6
harinikasturi <harinikasturi2003@gmail.com>
2025-04-29 12:15:56 +0530

    Day2 deleted from day3

--- day2/LEVEL1/Problem1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-	class Problem1{
-		public static void main(String[] args){
-			Scanner sn=new Scanner(System.in);
-			System.out.println("Enter the number");
-			int number=sn.nextInt();
-			System.out.println("Is the number " + number+ " divisible by 5?  "+(number%5==0));
-		}
-	}
